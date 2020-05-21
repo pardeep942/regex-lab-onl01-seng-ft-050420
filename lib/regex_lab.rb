7fb75c8f290f2034c2aac42d068ff683fc8e4214
PARDEEP KAUR <pkaur84@aol.com>
2020-05-21 00:19:14 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -9,7 +9,7 @@ text.scan (/un\w*ing/)
 end
 
 def words_five_letters_long(text)
-  text.scan (/\b {5}\b/)
+  text.scan (/\b {5}w\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
